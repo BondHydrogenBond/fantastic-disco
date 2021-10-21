14c2f7d9eee9b8f3d4585287b62d3df7c5d4b5a9
MarinaM <56633802+BondHydrogenBond@users.noreply.github.com>
2021-10-21 20:12:39 +0100

    Update ArrayFundamentalsPracticeQ1.cpp

--- ArrayFundamentalsPracticeQ1.cpp
+++ ArrayFundamentalsPracticeQ1.cpp
@@ -4,28 +4,25 @@
 
 using namespace std;
 
-/* comments for Adrian
-this is very basic, I'm iterating through the array and looking for the highest value,
-and comparing if this is the last value in the array (as it's ascending)
-*/
-
 const int testArraySize = 7;
-int testArray[testArraySize] = {1, 2, 10, 4, 5, 7, 8};
+int testArray[testArraySize] = {1, 2, 10, 4, 10, 7, 8};
 
 bool isSorted(int testArray[], int testArraySize)
 {
   bool isSorted = false;
   
-  int highestValue = testArray[0];
+  int arrayCount = 0;
 
   for (int i = 1; i < testArraySize; i++)
   { 
-    if (testArray[i] > highestValue)
-     {highestValue = testArray[i];} 
+    if (testArray[i] > testArray[i-1])
+     {arrayCount++;}
+
   }
   
-  if (highestValue == testArray[testArraySize-1])
-    {isSorted = true;}
+  
+  if (arrayCount== testArraySize-1)
+     {isSorted = true;}
    
   return isSorted;
 }
@@ -33,8 +30,8 @@ bool isSorted(int testArray[], int testArraySize)
 int main()
 {
 
-cout << "isSorted returned " << isSorted(testArray, testArraySize) << endl;
+ cout << "isSorted returned " << isSorted(testArray, testArraySize) << endl;
 
-return (0);
+ return (0);
 
 }
